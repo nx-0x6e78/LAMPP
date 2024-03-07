a7e8cb435bdc4901d48ff22c53a70975d1125565
keln <95649209+keln01@users.noreply.github.com>
2024-03-07 19:32:20 +0000

    Delete lampp directory

--- lampp/src/tomcatUninstallation.sh
+++
@@ -1,9 +0,0 @@
-sudo killall -u tomcat
-sudo userdel --remove -f tomcat
-sudo rm -rf /opt/tomcat
-
-sudo systemctl stop tomcat
-sudo rm -f /etc/systemd/system/tomcat.service
-
-sudo systemctl reset-failed
-sudo systemctl daemon-reload
